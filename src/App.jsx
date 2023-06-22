475e0f8f77ef66887eea1bc835c527744dcf4042
EshuShango <tobiakinyemi21@gmail.com>
2023-06-22 16:32:14 -0500

    simple function creeation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-  
 
+
+// const welcome = {
+//   greeting: 'Hey',
+//   title: 'React',
+// }
+const getTitle = (title) => {
+  return title
+}
+
+function App() {
+  // const [count, setCount] = useState(0)
+  
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* <h1> {welcome.greeting} {welcome.title}</h1> */}
+        <h1> {getTitle('Hey 💛')}</h1>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <label htmlFor="search">Search: </label>
+      <input type="text" />
+
+      {/* <button onClick={() => setCount(count + 1)}>Click me</button>
+      count is {count} */}
     </>
   )
 }
